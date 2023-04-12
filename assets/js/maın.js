96cb38cfc497c82ae3c275391a5abd2dd9905c55
Nilufar Najiyeva <127186140+nilufarnajiyeva@users.noreply.github.com>
2023-04-13 00:19:37 +0400

    Add files via upload

---
+++ assets/js/maın.js
@@ -0,0 +1,13 @@
+var herf= prompt("herf yaz")
+switch(herf){
+    case"a":
+    alert("Azərbaycan dili seçilir")
+    break;
+    case"r":
+    alert("Rus dili seçilir")
+    break;
+    default:
+        alert("yanlisdir");
+        break;
+    
+}
